71f26f427aff8f33d3575e5148add708f4b5d5bd
carrrllos <caedu762@gmail.com>
2025-01-14 16:20:48 -0300

    Exceptions

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardException extends RuntimeException{
+    private static final Long serialVersionUID = 1L;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+}
